d4ad3d3204d98be353ce6fbcf7448be657d282dc
Andrew Yaksic <nutclubflush@gmail.com>
2026-05-05 19:24:14 -0400

    Initial Commit

---
+++ lifetimer Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lifetimer Watch App
+//
+//  Created by Andrew Yaksic on 5/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lifetimer Watch App/lifetimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  lifetimerApp.swift
+//  lifetimer Watch App
+//
+//  Created by Andrew Yaksic on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct lifetimer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
