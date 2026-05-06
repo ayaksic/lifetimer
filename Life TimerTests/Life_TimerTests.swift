c41573e7b4b36fa415344f65390021c7e33a1741
Andrew Yaksic <nutclubflush@gmail.com>
2026-05-05 23:28:45 -0400

    Initial Commit

---
+++ Life TimerTests/Life_TimerTests.swift
@@ -0,0 +1,19 @@
+//
+//  Life_TimerTests.swift
+//  Life TimerTests
+//
+//  Created by Andrew Yaksic on 5/5/26.
+//
+
+import Testing
+@testable import Life_Timer
+
+struct Life_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
